24f539c7ccafe6f3270e3227f74164bb16b22de0
narasimha-darshan-ms <48201841+narasimha-darshan-ms@users.noreply.github.com>
2019-04-19 16:54:34 +0530

    dynamic programming

---
+++ LEET CODE/climbing_stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    
+    int climbStairs(int n) {
+        if(n == 1)  return 1;
+        if(n == 2)  return 2;
+        int arr[n];
+        memset(arr,-1,n);
+        if(n == 1)  return 1;
+        arr[0] = 1;
+        arr[1] = 2;
+        for(int i = 2 ; i < n ; i++)
+            arr[i] = arr[i-1] + arr[i-2];
+        return arr[n-1];
+    }
+};
